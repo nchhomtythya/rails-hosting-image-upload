ebf182ae21970f594862ba038f896d375344f63f
nchhomtythya <nctythya@outlook.com>
2022-11-18 17:17:36 +1100

    scaffold article and its working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, except: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "articles#index
-  resources :articles, except: :index
+  root to: "articles#index"
 
 end
